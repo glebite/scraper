6fde1cc1494fc480d2c49e74592857cce14dbd7b
glebite <glebite@gmail.com>
2021-08-21 22:46:21 -0400

    Comments...

--- src/common.py
+++ src/common.py
@@ -15,13 +15,19 @@ PRIVACY_BUTTON = 'lds__privacy-policy__btnClose'
 
 class Common(object):
     def __init__(self, config_file=None):
+        """
+        """
         self.driver = webdriver.Firefox()
         self.driver.get(LOCATION)
 
     def __del__(self):
+        """
+        """
         self.driver.close()
 
     def find(self, tag, tag_class):
+        """
+        """
         d = self.driver.find_element_by_class_name(PRIVACY_BUTTON)
         print(d)
         if d:
